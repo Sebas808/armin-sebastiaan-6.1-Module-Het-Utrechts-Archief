7f1b27686c150754efed72a83167ffbd2bcdf6bd
Sebas808 <Sebastiaan0207@gmail.com>
2025-12-05 11:59:03 +0100

    push

--- home.php
+++ home.php
@@ -9,7 +9,30 @@
 <body>
   <?php  include 'includes/header.php'; ?>
 <div class="home-container">
-    <?php  include 'main.php'; ?>
+<section class="home-welcome">
+<div class="home-welcome-container">
+<h1 class="home-welcome-title" data-i18n="welcome_title">Welkom bij Het Utrechts Archief</h1>
+
+
+<p class="home-welcome-intro" data-i18n="welcome_intro">
+Ontdek de verhalen, details en verborgen parels van het historische Utrecht. Onze digitale panorama neemt je mee
+terug in de tijd en laat je de stad ervaren zoals zij er ooit uitzag.
+</p>
+
+
+<div class="home-welcome-highlight">
+<p data-i18n="welcome_highlight">
+Verken iconische locaties, zoom in op bijzondere elementen en laat je inspireren door het rijke erfgoed dat
+Utrecht al eeuwenlang vormt.
+</p>
+</div>
+
+
+<p class="home-welcome-subtext" data-i18n="welcome_subtext">
+Scroll naar beneden om de volledige panorama te bekijken en jouw reis door het oude Utrecht te beginnen.
+</p>
+</div>
+</section>
 </div>
 
 
